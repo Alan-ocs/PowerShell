4ad67ab995b1d7a4b36411d954641bb41be6bfbe
Alan Silva <80364531+Alan-ocs@users.noreply.github.com>
2021-03-12 14:34:19 -0300

    Add files via upload

---
+++ Last 24 Hours Error Events of a Remote Machine.ps1
@@ -0,0 +1,2 @@
+﻿$ServerName = "HOSTNAME" #Modify with your ServerName
+Get-eventlog -ComputerName $ServerName -LogName System -EntryType Error -After (Get-Date).AddDays(-1) | select EventID, TimeGenerated, Message
