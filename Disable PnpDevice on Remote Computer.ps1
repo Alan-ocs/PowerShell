dbee02d5961670e6a3b778a524ef82418bb16f07
Alan Silva <80364531+Alan-ocs@users.noreply.github.com>
2021-03-12 13:47:53 -0300

    Add files via upload

---
+++ Disable PnpDevice on Remote Computer.ps1
@@ -0,0 +1,4 @@
+﻿Invoke-Command -ComputerName HOSTNAME -ScriptBlock {
+Get-PnpDevice | Where-Object {$_.FriendlyName -like '*touch screen*'} | Disable-PnpDevice -Confirm:$false} 
+
+# Modify the HOSTNAME with the remote computer name.
