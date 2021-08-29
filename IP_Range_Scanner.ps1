8b12e80cc67f88db78aa3c1ea880e6c4d5c45249
Alan Silva <alan.ocs@gmail.com>
2021-08-28 22:19:01 -0300

    Updates

--- IP_Range_Scanner.ps1
+++ IP_Range_Scanner.ps1
@@ -1,1 +1,3 @@
 1..254 | ForEach-Object {Get-WmiObject Win32_PingStatus -Filter "Address='192.168.0.$_' and Timeout=200 and ResolveAddressNames='true' and StatusCode=0" | select ProtocolAddress*}
+
+# Change the Address range to yours.
--- Search and Delete by Extension.ps1
+++ Search and Delete by Extension.ps1
@@ -1,1 +1,3 @@
 ﻿Get-ChildItem c:\ -include *.dmp -Recurse | foreach { Remove-Item -Path $_.FullName -WhatIf}
+
+# Change the *.dmp for the format that you want to search and remove
